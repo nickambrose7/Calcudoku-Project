31c11cd4468b28dc73cdfa00b56957ebdd2c0902
nickambrose7 <73966211+nickambrose7@users.noreply.github.com>
2021-02-10 08:40:42 -0800

    Finished check_column
    I wrote and tested the check_column function.

--- funcs_tests.py
+++ funcs_tests.py
@@ -5,23 +5,23 @@ class TestCases(unittest.TestCase):
       
    # ADD MORE TESTS!!!! 
 
-   def test_check_row1(self):
+   def test_check_row0(self):
       row = [1, 2, 3, 4, 5]
       self.assertTrue(check_row(row), True)
 
-   def test_check_row2(self):
+   def test_check_row1(self):
       row = [1, 2, 1, 4, 5]
       self.assertFalse(check_row(row), False)
 
-   def test_check_row3(self):
+   def test_check_row2(self):
       row = [1, 0, 0, 0, 0]
       self.assertTrue(check_row(row), True)
 
-   def test_check_row4(self):
+   def test_check_row3(self):
       row = [1, 1, 1, 1, 0]
       self.assertFalse(check_row(row), False)
 
-   def test_check_row5(self):
+   def test_check_row4(self):
       row = [1, 2, 6, 4, 5]
       self.assertFalse(check_row(row), False)
 
@@ -63,6 +63,58 @@ class TestCases(unittest.TestCase):
       puzzle.append([3, 3, 0, 1, 2])
       puzzle.append([0, 0, 0, 0, 0])
       self.assertFalse(check_rows_valid(puzzle), False)
+
+
+
+   def test_check_column0(self):
+      puzzle = []
+      puzzle.append([5, 1, 2, 3, 4])
+      puzzle.append([1, 2, 3, 4, 5])
+      puzzle.append([2, 3, 0, 5, 1])
+      puzzle.append([3, 0, 0, 1, 2])
+      puzzle.append([0, 0, 0, 0, 0])
+      self.assertTrue(check_column(puzzle, 0), True)
+
+   def test_check_column1(self):
+      puzzle = []
+      puzzle.append([5, 1, 2, 3, 4])
+      puzzle.append([1, 2, 3, 4, 5])
+      puzzle.append([2, 3, 0, 5, 1])
+      puzzle.append([1, 0, 0, 1, 2])
+      puzzle.append([0, 0, 0, 0, 0])
+      self.assertFalse(check_column(puzzle, 0), False)
+
+   def test_check_column2(self):
+      puzzle = []
+      puzzle.append([0, 1, 1, 3, 4])
+      puzzle.append([1, 2, 3, 4, 5])
+      puzzle.append([2, 3, 0, 5, 1])
+      puzzle.append([3, 0, 0, 1, 2])
+      puzzle.append([0, 0, 0, 0, 0])
+      self.assertTrue(check_column(puzzle, 0), True)
+
+   def test_check_column3(self):
+      puzzle = []
+      puzzle.append([5, 1, 2, 3, 4])
+      puzzle.append([1, 2, 3, 4, 5])
+      puzzle.append([2, 3, 0, 5, 1])
+      puzzle.append([1, 0, 0, 1, 1])
+      puzzle.append([0, 0, 0, 0, 1])
+      self.assertFalse(check_column(puzzle, 4), False)
+
+   def test_check_column4(self):
+      puzzle = []
+      puzzle.append([0, 0, 0, 0, 0])
+      puzzle.append([0, 0, 0, 0, 0])
+      puzzle.append([0, 0, 0, 1, 0])
+      puzzle.append([0, 0, 0, 1, 0])
+      puzzle.append([0, 0, 0, 0, 0])
+      self.assertFalse(check_column(puzzle, 3), False)
+
+
+
+
+
 
 
 
--- solverFuncs.py
+++ solverFuncs.py
@@ -8,22 +8,22 @@ def check_rows_valid(puzzle):
 	for i in range(len(puzzle)):
 		
 		if check_row(puzzle[i]):
+			
 			return True
+		
 		else:
+			
 			return False
-		
 
-	
-
-		
-
-   
        
 def check_row(row):
 	
 	for i in range(len(row)):
+		
 		for j in range(len(row)):
+			
 			if row[i] == row[j] and i != j and row[i] != 0 and row[j] != 0 or row[i] > 5 and row[j] > 5: #compares every number in the list and ensures there are no duplicates AT DIFFERENT INDEXES. Excludes 0. returns False if value in row is greater than 5
+				
 				return False
 
 	return True
@@ -33,7 +33,26 @@ def check_columns_valid(puzzle):
    pass
 
 def check_column(puzzle, col_num):
-	pass
+
+	col_list = []
+
+	for i in range(len(puzzle)): #goes through each row
+		
+		item = puzzle[i][col_num] #pulls out the number in the specified column for each row
+		
+		col_list.append(item) #poplulates list with all numbers from specified column
+
+	for j in range(len(col_list)): #Same logic as check row except it uses the list of a column now.
+
+		for k in range(len(col_list)):
+
+			if col_list[j] == col_list[k] and j != k and col_list[j] != 0 and col_list[k] != 0 or col_list[j] > 5 and col_list[k] > 5:
+
+				return False
+
+	return True
+	
+	
 
 
 def check_cages_valid(puzzle, cages):
